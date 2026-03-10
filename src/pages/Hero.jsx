95c16a361ed3026d702ead7f3f725c317fb45390
Aryanraval2030 <aryanraval1432@gmail.com>
2026-03-10 21:19:12 +0530

    structure

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -13,8 +13,19 @@ function Hero() {
           mouseInteractive={true}
         />
       </div>
-      <div className="absolute top-[100px] pointer-events-none left-[0] w-[100vw] mt-[10px]">
-       
+      <div className="absolute top-[100px] pointer-events-none left-[0] w-[100vw] mt-[10px] text-center">
+        <div className="">
+          <h1 className="leading-[10rem] text-4xl text-[#3b82f6]">
+            Connect with Developers Worldwide
+          </h1>
+          <p className="text-4xl text-[#3b82f6]">
+            Share your projects, follow developers, and grow together
+          </p>
+        </div>
+        <div className="mt-[8vh] flex gap-5 justify-center">
+          <button className="border-2 border-[#3b82f6] text-2xl py-2 rounded-full bg-[#0f172a] text-[white] px-10">sign-up</button>
+          <button className="border-2 border-[#3b82f6] text-2xl py-2 rounded-full bg-[#0f172a] text-[white] px-10">explore</button>
+        </div>
       </div>
     </>
   );
